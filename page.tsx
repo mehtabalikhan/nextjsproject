daab96fe075e07a1e7b2765b97e152dd4ed7b694
mehtabalikhan <165506562+mehtabalikhan@users.noreply.github.com>
2024-11-10 12:43:24 -0600

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{display:"flex",flexDirection:"column",alignItems:"center",height:"100vh"
+    }}>
+    <h1 style={{color:"blue",fontSize:"3rem"}}>Hello World!!!</h1>
+    </div>
+  );
+}
